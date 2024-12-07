f198df2519f3af533448f8b57173ae75b0495f14
dawsthehassell <hasselldawson@gmail.com>
2024-12-07 14:17:50 -0600

    print character_count

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    lowered_string = text.lower()
+    character_counts = {}
+    for string in lowered_string:
+        if string in character_counts:
+            character_counts[string] += 1
+        else:
+            character_counts[string] = 1
+    return character_counts
+
 file_contents = main()
-result = word_count(file_contents)
-print(result)
+character_counts = count_characters(file_contents)
+print(character_counts)
+
